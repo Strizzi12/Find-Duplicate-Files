3f12bf6471da00109b6ae34d20c93a1f7da86a46
Unknown <andreas-reschenhofer@gmx.at>
2017-12-20 00:32:08 +0100

    Get files from given directory
    This includes folder depth, all given file paths and file filters.

---
+++ FindDuplicateFiles/Controller.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace FindDuplicateFiles {
+	public class Controller {
+		private bool _waitForTermination;
+		private bool _printProcessTime;
+		private bool _optimizeTaskCount;
+		private bool _error;
+		private List<string> _filePaths;
+		private bool _moreInfo;
+		private List<string> _fileFilter; //already a regex pattern
+		private int? _depthOfRecursion;
+		private int? _maxTasks;
+
+		public List<string> GetFilesForAllPaths() {
+			List<string> allFiles = new List<string>();
+			foreach (var filePath in _filePaths) {
+				var list = GetFiles(filePath, _fileFilter, _depthOfRecursion);
+				allFiles.AddRange(list);
+			}
+			return allFiles;
+		}
+
+		private List<string> GetFiles(string path, List<string> filter, int? depth = null) {
+			var files = new List<string>();
+			string[] directoriesInPath;
+			string[] filesInPath;
+			try {
+				directoriesInPath = Directory.GetDirectories(path);
+				filesInPath = Directory.GetFiles(path);
+			} catch (Exception ex) {
+				Console.WriteLine(ex);
+				return new List<string>();
+			}
+			foreach (var directory in directoriesInPath) {
+				if (depth != null) {
+					files = GetFiles(directory, filter, depth - 1);
+				} else {
+					files = GetFiles(directory, filter);
+				}
+			}
+			files.AddRange(filesInPath);
+			return FilterFiles(files, filter);
+		}
+
+		private List<string> FilterFiles(List<string> files, List<string> filters) {
+			var list = new List<string>();
+			if (filters.Count > 0) {
+				foreach (var file in files) {
+					foreach (var filter in filters) {
+						var regex = new Regex(filter);
+						if (regex.IsMatch(file)) {
+							list.Add(file);
+							break;
+						}
+					}
+				}
+				return list;
+			}
+			return files;
+		}
+
+		public void ParseInputArguments(string[] argv) {
+			for (int i = 0; i < argv.Length; i++) {
+				switch (argv[i]) {
+					case "-w":
+						_waitForTermination = true;
+						continue;
+					case "-p":
+						_printProcessTime = true;
+						continue;
+					case "-t":
+						if (argv[i + 1] != null) {
+							_optimizeTaskCount = true; //When file size is known, a calculation of the optimal thread count could be made.
+							continue;
+						}
+						if (IsDigitsOnly(argv[i + 1])) {
+							_maxTasks = Int32.Parse(argv[i + 1]);
+							i++; //Counter can be increased because the value of maxThreads is already read.
+							continue;
+						} else {
+							Console.WriteLine("Error! Max. thread count must not contain character");
+							_error = true;
+							break;
+						}
+					case "-h":
+						PrintHelp();
+						continue;
+					case "-v":
+						_moreInfo = true;
+						continue;
+					case "-f":
+						if (argv[i + 1] != null) {
+							var completeString = argv[i + 1];
+							//Split string at ;
+							var allFilters = completeString.Split(';');
+							foreach (var filter in allFilters) {
+								var temp = filter.Replace("*", @"([a-zA-Z0-9\._\-]*)");
+								_fileFilter.Add(temp);
+							}
+							i++; //Counter can be increased because the value of fileFilter is already read.
+							continue;
+						} else {
+							Console.WriteLine("Error! No filter given");
+							_error = true;
+							break;
+						}
+					case "-s":
+						_filePaths.Add(argv[i + 1]);
+						i++; //Counter can be increased because the value of filePath is already read.
+						continue;
+					case "-r":
+						if (argv[i + 1] != null && !argv[i + 1].Contains("-")
+						) //Check if the next input argument is another functionality.
+						{
+							_depthOfRecursion = null; //All files and folders are progressed
+							continue;
+						} else {
+							var help = argv[i + 1];
+							if (IsDigitsOnly(help)) {
+								_depthOfRecursion = Int32.Parse(help);
+								i++; //Counter can be increased because the value of maxThreads is already read.
+								continue;
+							}
+							Console.WriteLine("Error! Folder depth must not contain character.");
+							_error = true;
+							break;
+						}
+					default:
+						Console.WriteLine("Error! Unknown argument detected.");
+						_error = true;
+						break;
+				}
+			}
+		}
+
+		private static void PrintHelp() {
+			Console.WriteLine("Die Applikation kann wie folgt aufzurufen:");
+			Console.WriteLine("cntFileBits [-r [n]] [-f fileFilter] [-t maxThreads] [-h] [-p] [-v] [-w] [-s startPath]");
+			Console.WriteLine("-s startPath	Gibt das Startverzeichnis an, ab dem die Dateien gelesen werden sollen;");
+			Console.WriteLine("die Option -s kann auch mehrfach angegeben werden, z.B. wenn zwei Partitionen durchsucht werden sollen");
+			Console.WriteLine("-r [n] Rekursives Lesen der Unterverzeichnisse; wenn n (bei n >= 1) angegeben, dann");
+			Console.WriteLine("bestimmt n die Tiefe der Rekursion; wird n nicht angegeben, dann werden");
+			Console.WriteLine("rekursiv alle unter dem Startverzeichnis stehenden Verzeichnisse und deren Dateien gelesen;");
+			Console.WriteLine("-f fileFilter fileFilter gibt an, welche Dateien gelesen werden sollen; z.B. *.iso oder bild*.jpg;");
+			Console.WriteLine("wird diese Option nicht angegeben, so werden alle Dateien gelesen;");
+			Console.WriteLine("-t maxThreads maximale Anzahl der Threads; wird diese Option nicht angegeben, dann wird die Anzahl der Threads automatisch optimiert.");
+			Console.WriteLine("-h Anzeige der Hilfe & Copyright Info; wird automatisch angezeigt, wenn beim Programmstart keinen Option angegeben wird.");
+			Console.WriteLine("-p Ausgabe der Prozesserungszeit auf stdout in Sekunden.Millisekunden");
+			Console.WriteLine("-v Erweiterte Ausgabe etwaiger Prozessierungsinformationen auf stdout");
+			Console.WriteLine("-w Warten auf eine Taste unmittelbar bevor die applikation terminiert.");
+			Console.WriteLine("Copyright© by Mike Thomas and Andreas Reschenhofer");
+		}
+
+		private static bool IsDigitsOnly(string str) {
+			foreach (char c in str) {
+				if (c < '0' || c > '9')
+					return false;
+			}
+			return true;
+		}
+	}
+}
---
+++ FindDuplicateFiles/FindDuplicateFiles.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindDuplicateFiles {
+	class FindDuplicateFiles {
+
+		private static readonly string path = @"C:\Users\Andi\Downloads";
+		private static readonly string path2 = @"C:\Users";
+		
+
+		static void Main(string[] args) {
+			var dict = new ConcurrentDictionary<long, List<string>>();
+			var controller = new Controller();
+			var watch = System.Diagnostics.Stopwatch.StartNew();
+
+			//Get all Files from directory and subdirectories 
+			
+			//TODO: Access restriction
+			var files = controller.GetFilesForAllPaths();
+			
+
+			//Sort files by filesize into dictionary
+			foreach (var filePath in files) {
+				long fileSize = new FileInfo(filePath).Length;
+				List<string> list;
+				if (dict.TryGetValue(fileSize, out list)) {
+					list.Add(filePath);
+				} else {
+					List<string> listTemp = new List<string>();
+					listTemp.Add(filePath);
+					dict.GetOrAdd(fileSize, listTemp);
+				}
+			}
+
+			//Delete entries in dict where count == 1
+			foreach (var toDelete in dict.Where(v => v.Value.Count == 1).ToList()) {
+				List<string> list;
+				dict.TryRemove(toDelete.Key, out list);
+			}
+
+			watch.Stop();
+			var elapsedMs = watch.ElapsedMilliseconds;
+			Console.WriteLine(elapsedMs + "ms");
+			Console.ReadLine();
+		}
+
+		void ParseInputArguments(string[] argv) {
+			
+		}
+		
+
+		
+	}
+}
